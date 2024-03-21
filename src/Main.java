da9f63911ca3d388e8badf9f182cbb4eef2fc552
Cris <c.cheuquel01@ufromail.cl>
2024-03-21 10:16:14 -0300

    Actividad 6.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
         menu();
@@ -8,7 +6,13 @@ public class Main {
         double[][] sismos = new double[24][7];
         System.out.println("Monitoreo Sismográfico");
         ingresarDatos(sismos);
-        System.out.println(buscarPorMagnitud(sismos));
+        System.out.printf("Maximo: %.1f", buscarPorMagnitud(sismos));
+        System.out.println("");
+        System.out.println("Cantidad mayores a 5.0: " + contar(sismos, 5.0));
+        for(int i = 0; i < contar(sismos, 7.0); i++){
+            enviarSMS();
+        }
+
     }
     public static double generarRandomDouble(){
         double MIN = 0.0;
@@ -16,8 +20,6 @@ public class Main {
         return MIN + (Math.random() * (MAX - MIN - 0.1));
     }
     public static void ingresarDatos(double[][] sismos){
-
-
         for(int i = 0; i < 24; i++){
             for(int j = 0; j < 7; j++) sismos[i][j] = generarRandomDouble();
         }
@@ -35,4 +37,26 @@ public class Main {
         }
         return max;
     }
+    public static boolean validarMagnitud(double magnitud, double condicion){
+
+        if(magnitud > 7.7){
+            return true;
+        }
+        return false;
+    }
+    public static void enviarSMS(){
+        System.out.println("ALERTA: SISMO DE MAGNITUD ALTA !!!");
+    }
+    public static int contar(double[][] sismos, double condicion) {
+        int cantidad = 0;
+        for (int i = 0; i < 24; i++) {
+            for (int j = 0; j < 7; j++) {
+                if(validarMagnitud(sismos[i][j], condicion)){
+                    cantidad++;
+                }
+            }
+        }
+        return cantidad;
+    }
+
 }
